4185826589aa9ac653d13f7eff7e3212783f25f5
krzysiek <k.spetkowski@wp.pl>
2014-11-29 11:44:13 +0100

    migrations added

---
+++ app/DoctrineMigrations/Version20141129102110.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20141129102110 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('CREATE TABLE Job (id INT AUTO_INCREMENT NOT NULL, campanyName VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, type SMALLINT NOT NULL, logo VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, position VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, howToApply VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('DROP TABLE Job');
+    }
+}
