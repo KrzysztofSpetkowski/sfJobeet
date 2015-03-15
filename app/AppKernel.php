603993f98a75e5cd7836d81eb13d09533864472b
krzysiek <k.spetkowski@wp.pl>
2015-03-15 22:29:03 +0100

    added new bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle,
             new Admingenerator\GeneratorBundle\AdmingeneratorGeneratorBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new Bootcamp\UserBundle\BootcampUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Bootcamp/UserBundle/BootcampUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bootcamp\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BootcampUserBundle extends Bundle
+{
+}
