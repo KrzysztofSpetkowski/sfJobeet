35fd229e543ed9beb04eb70d5cea2ba682b27409
krzysiek <k.spetkowski@wp.pl>
2015-04-07 23:01:38 +0200

    new jobs on the home page

--- src/Bootcamp/JobeetBundle/Controller/DefaultController.php
+++ src/Bootcamp/JobeetBundle/Controller/DefaultController.php
@@ -7,29 +7,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller 
 
 {
-   public function indexAction($page=1)
-{
-//    $em    = $this->get('doctrine.orm.entity_manager');
-//    $sql   = "SELECT created_at FROM Job ";
-//    $query = $em->createQuery($sql);
-//
-//    $paginator  = $this->get('knp_paginator');
-//    $pagination = $paginator->paginate(
-//        $query,
-//        $job,
-//        5/*limit per page*/
-//    );
-        $sql = "SELECT * FROM Job WHERE created_at";
-    // parameters to template
-    return $this->render('BootcampJobeetBundle:Default:index.html.twig', array('page' => $page));
-}
+    public function indexAction($page=1)
+    {
+    // wyszukiwanie 5 najnowszych rekordów
+        
+    $jobrepository=$this->getDoctrine()->getRepository('BootcampJobeetBundle:Job');
+        $query = $jobrepository->createQueryBuilder('p')
+            ->setMaxResults(5)
+            ->orderBy('p.createdAt', 'desc')
+            ->getQuery();
+        $jobs = $query->getResult();
+
+        return $this->render('BootcampJobeetBundle:Default:index.html.twig', array(
+            'jobs' => $jobs,
+            'page' => $page
+       
+        ));
+    }
     public function dealsAction($page=1)
         {
         $jobs = $this->getDoctrine()
     		->getRepository('BootcampJobeetBundle:Job')
     		->findAll();
-    	
-    	   // return $this->redirect($this->generateUrl('bootcamp_panel_list'));
+
     	$paginator  = $this->get('knp_paginator');
         $pages = $paginator->paginate(
             $jobs,
